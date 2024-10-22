da4a36fba77d4df0cf8ac8b228b4ea44945a86bd
Michael Leonhard <michael@leonhardllc.com>
2024-10-21 23:50:39 -0700

    Use std::cell::OnceCell instead of once_cell::sync::Lazy.

--- src/util/random.rs
+++ src/util/random.rs
@@ -1,14 +1,16 @@
 use nanorand::{ChaCha, Rng};
-use once_cell::sync::Lazy;
+use std::cell::OnceCell;
 use std::sync::Mutex;
 
 /// Cryptographically-secure Pseudo-Random Number Generator
-pub static CSPRNG: Lazy<Mutex<ChaCha<20>>> = Lazy::new(|| Mutex::new(ChaCha::new()));
+pub static CSPRNG: Mutex<OnceCell<ChaCha<20>>> = Mutex::new(OnceCell::new());
 
 #[must_use]
+#[allow(clippy::missing_panics_doc)]
 pub fn csprng_random_positive_nonzero_i64() -> i64 {
-    CSPRNG
+    let mut guard = CSPRNG
         .lock()
-        .unwrap_or_else(std::sync::PoisonError::into_inner)
-        .generate_range(1_i64..i64::MAX)
+        .unwrap_or_else(std::sync::PoisonError::into_inner);
+    guard.get_or_init(ChaCha::new);
+    guard.get_mut().unwrap().generate_range(1_i64..i64::MAX)
 }
